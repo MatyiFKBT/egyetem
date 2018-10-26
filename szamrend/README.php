cb4a8dc7ddb3f8a43b6c441b43e6d769a33aa6c4
MatyiFKBT <mmatyi1999@gmail.com>
2018-10-26 11:15:31 +0200

    Update README.php

--- szamrend/README.php
+++ szamrend/README.php
@@ -1,9 +1,15 @@
-| Hét | Dátum | Órai munka | Forrás |
-|-------|-------|-------------------------------------|-------------------------------------|
-| 1.hét |  |  |  |
-| 2.hét |  |  |  |
-| 3.hét |  |  |  |
-| 4.hét |  |  |  |
-| 5.hét |  |  |  |
-| 6.hét |  |  |  |
-| 7.hét | 10.24 | https://SzamRend1024.matyi.repl.run | https://repl.it/@matyi/SzamRend1024 |
+<ul>
+
+  <li>Tantárgy neve (jelen esetben Progalap a teszt)<br>
+  </li>
+  <ul>
+    <li><a href="https://repl.it/@matyi/Progalap1">teszt házi</a><br>
+    </li>
+  </ul>
+  <ul>
+<?php for ($i = 1; $i < 10; $i++) {
+    echo "<li><a href=\"https://repl.it/@matyi/Progalap$i\">$i. házi</a><br></li>";
+  }
+  ?>
+  </ul>
+</ul>
