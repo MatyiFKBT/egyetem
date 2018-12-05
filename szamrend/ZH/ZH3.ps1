4c8d8cf20eca4d96038a72b4ec9d60f671865824
MatyiFKBT <mmatyi1999@gmail.com>
2018-12-05 15:37:47 +0100

    Add files via upload

---
+++ szamrend/ZH/ZH3.ps1
@@ -0,0 +1,18 @@
+﻿# Valósítsd meg a következő függvényeket:
+# A feladat során VÉGIG egy zh3.ps1 nevű fileban dolgozz, ha végeztél, akkor ZH3 tárggyal, A levlétörzsben NÉV+Neptunkóddal küldd el a nebsadi@inf.elte.hu címre.  
+
+# 1) Nagyobb függvény:
+# Két paramétert vár, visszatérési értéke a két szám közül a nagyobb értéke, Ha csak egy paramétert kap, akkor annak az értékével.
+# Használj paraméter ellenőrzést//típuskényszert hogy jobban működjön a függvény hibás paraméterrel. 
+
+# 2) Arlista függvény:
+# Beolvas egy file-t amiben [Név] [Ár] [Db] formában vannak tárolva az összetartozó adatok, soronként.
+# Tároljuk el ezeket objektumban, a [Db] nélkül, és térjünk vissza vele. 
+
+# 3) Sarkany függvény:
+# Írja hozzá a karok.txt-hez a következő sort: 
+# "A sari lebontott (n) tornyot "
+# (n) legyen egy véletlen szám 1 és 10 között. 
+
+# 4) OkozottKarok függvény:
+# A fenti függvény által módosított file segítségével határozza meg, hány tornyot bontott le a sárkány.
